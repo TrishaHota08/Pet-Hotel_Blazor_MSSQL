4a174c475bdeae8c9b521eea637f5dc3580faee7
Trisha Hota <trisha.hota@gmail.com>
2023-06-25 13:13:35 +0400

    Add project files.

---
+++ PetHotelDomain/Owner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHotelDomain
+{
+    public class Owner
+    {
+        public Owner() 
+        {
+            Cats = new List<Cats>();
+        }  
+        public int ownerId { get; set; }
+        public string ownerName { get; set; }
+        public string ownerEmail { get; set; }
+        public string ownerPhone { get; set; }
+
+        public List<Cats> Cats { get; set; }
+
+    }
+}
